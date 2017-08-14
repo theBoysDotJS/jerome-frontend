08b0b84be1707b9fda64c94b9c3f143eac1591e3
Quinn Langille <quinn@quinnlangille.com>
2017-08-14 18:50:28 -0400

    added files from hao's repo

---
+++ src/components/pages/Login.js
@@ -0,0 +1,32 @@
+import React from 'react';
+//https://www.npmjs.com/package/react-facebook-login
+import FacebookLogin from 'react-facebook-login';
+
+export default class Login extends React.Component {
+  constructor(props) {
+    super();
+  }
+
+
+
+  render() {
+    return (
+    <div>
+      <div id="fb">
+        login with facebook:
+        <FacebookLogin
+        appId="1311437462287493"
+        autoLoad={true}
+        fields="name,email,picture"
+        />
+
+        <div id="login">
+          <input type="text" ref="email"/>
+          <input type="password" ref="password"/>
+          <button onClick={this._handleLogin}>login</button>
+        </div>
+      </div>
+    </div>
+    )
+  }
+}
