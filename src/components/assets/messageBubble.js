815fafbf99f741385636e44bb82bb428cf7b55ed
Quinn Langille <quinn@quinnlangille.com>
2017-08-14 19:05:04 -0400

    made bubble, changed component names to uppercase

---
+++ src/components/assets/messageBubble.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class MessageBubble extends React.Component {
+
+	render() {
+		return(
+			<div id="message-bubble">
+				<p></p>
+			</div>
+		);
+	}
+}
+
+export default MessageBubble;
