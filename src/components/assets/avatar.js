05c2ab24899ebb995bb9100d5ec3f8dc1d69830f
Mike McDonald <mikemcd97@gmail.com>
2017-08-14 18:46:55 -0400

    built skeleton of avatar

---
+++ src/components/assets/avatar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class avatar extends Component {
+  render() {
+    return (
+      <div className="avatar">
+      <img alt="avatar" href=`https://www.gravatar.com/avatar/${hash}?d=wavatar`
+
+      </div>
+    );
+  }
+}
+
+export default App;
