6fa959e0158ae8175481470e68264e0d3b13a7e6
Tim McNamara <timlmcn@gmail.com>
2017-08-22 14:48:50 -0400

    change-lang

--- src/components/assets/LanguageForm.js
+++ src/components/assets/LanguageForm.js
@@ -1,20 +1,40 @@
 import React from 'react';
 
 class LanguageForm extends React.Component {
+	constructor() {
+		super()
+		this.state = {
+			value : ''
+		}
+	}
+
+	change = (e) => {
+		this.setState({ value : e.target.value })
+	}
+
+	saveUserLanguage = (lang) => {
+		fetch()
+		.then()
+		.catch()
+	}
 
 	render() {
+
 		return (
-			<select onChange={e => this.props.language(e, this.menu.value)} name="languageForm" id="langaugeForm" ref={(input) => this.menu = input}>
-				<option value="en">English</option>
-				<option value="fr">French</option>
-				<option value="es">Spanish</option>
-				<option value="pt">Portugese</option>
-				<option value="de">German</option>
-				<option value="it">Italian</option>
-				<option value="hi">Hindi</option>
-				<option value="ar">Arabic</option>
-				<option value="ru">Russian</option>
-			</select>
+			<div>
+				<select onChange={this.change} value={this.state.value} name="languageForm" id="langaugeForm">
+					<option value="en">English</option>
+					<option value="fr">French</option>
+					<option value="es">Spanish</option>
+					<option value="pt">Portugese</option>
+					<option value="de">German</option>
+					<option value="it">Italian</option>
+					<option value="hi">Hindi</option>
+					<option value="ar">Arabic</option>
+					<option value="ru">Russian</option>
+				</select>
+				<p>{this.state.value}</p>
+			</div>
 		)
 	}
 }
